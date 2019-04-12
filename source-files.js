af19f5f11edc031d3a97a943b58d7de4a556124f
Travis CI User <travis@example.org>
2019-04-12 05:57:05 +0000

    Update documentation

---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["hyperlocal"] = {"name":"","dirs":[{"name":"client","dirs":[],"files":["mod.rs"]},{"name":"server","dirs":[],"files":["mod.rs"]}],"files":["lib.rs"]};
+createSourceSidebar();
